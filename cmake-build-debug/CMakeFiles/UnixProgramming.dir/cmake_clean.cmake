2a5b7109709c6545c2a614969375ffef4bcead59
Jingyi Cui <jingyi.cui1@ucdconnect.ie>
2022-02-20 13:04:00 +0000

    final edition

---
+++ cmake-build-debug/CMakeFiles/UnixProgramming.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnixProgramming.dir/Assign1-19204584.c.o"
+  "UnixProgramming"
+  "UnixProgramming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UnixProgramming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/head2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/head2.dir/Assign1-19204584.c.o"
+  "head2"
+  "head2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/head2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
